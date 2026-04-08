3eff0dba2381c9b8d8e4a580f66dbd9aa8d4961b
nzinterbuu <nz.inter.buu@gmail.com>
2026-04-08 14:06:47 +0300

    Enhance widget functionality by adding advanced settings section and backend URL input. Update manifest to include new location and localization files for advanced settings titles in English and Russian. Remove outdated README file to streamline documentation.

--- apps/widget/script.js
+++ apps/widget/script.js
@@ -2,6 +2,34 @@ define(['jquery'], function () {
   'use strict';
 
   return function () {
+    var self = this;
+
+    function readBackendUrl() {
+      if (!self.get_settings) {
+        return '';
+      }
+      var settings = self.get_settings() || {};
+      if (settings.backend_url) {
+        return String(settings.backend_url);
+      }
+      if (settings.fields && settings.fields.backend_url) {
+        return String(settings.fields.backend_url);
+      }
+      return '';
+    }
+
+    function tr(path, fallback) {
+      var parts = path.split('.');
+      var node = self.langs || {};
+      for (var i = 0; i < parts.length; i += 1) {
+        if (!node || typeof node !== 'object' || !(parts[i] in node)) {
+          return fallback;
+        }
+        node = node[parts[i]];
+      }
+      return typeof node === 'string' ? node : fallback;
+    }
+
     this.callbacks = {
       init: function () {
         return true;
@@ -14,6 +42,26 @@ define(['jquery'], function () {
       },
       settings: function () {
         return true;
+      },
+      advancedSettings: function () {
+        var title = tr('advanced.title', 'Settings');
+        var backendLabel = tr('settings.backend_url', 'Backend URL');
+        var backendUrl = readBackendUrl();
+        var html =
+          '<div class="edna-advanced-settings">' +
+          '<div style="font-weight:600;margin-bottom:8px;">' +
+          title +
+          '</div>' +
+          '<div class="widget_settings_block__item_field">' +
+          '<label class="widget_settings_block__title_field">' +
+          backendLabel +
+          '</label>' +
+          '<input type="text" name="backend_url" value="' +
+          backendUrl.replace(/"/g, '&quot;') +
+          '" class="widget_settings_block__input_field" />' +
+          '</div>' +
+          '</div>';
+        return html;
       },
       onSave: function () {
         return true;
